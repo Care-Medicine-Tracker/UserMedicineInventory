fe7d540d57670cfecb5ad1e3925f20b2e1e0a00f
vcaf <victoria.fong.0408@gmail.com>
2023-01-15 14:42:10 +0100

    added performance tests

---
+++ src/Tests/Performance Tests/soak.js
@@ -0,0 +1,20 @@
+﻿import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '2m', target: 400 }, // ramp up to 400 users
+        { duration: '3h56m', target: 400 }, // stay at 400 for ~4 hours
+        { duration: '2m', target: 0 }, // scale down. (optional)
+    ],
+};
+const API_BASE_URL  = 'http://20.103.147.242';
+//insert id to be tested on
+const userId  = '';
+
+export default function () {
+    // Here, we set the endpoint to test.
+    http.get(`${API_BASE_URL}/prescription/user/${userId}`);
+
+    sleep(1);
+}
---
+++ src/Tests/Performance Tests/stress-test.js
@@ -0,0 +1,30 @@
+﻿import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '2m', target: 100 }, // below normal load
+        { duration: '5m', target: 100 },
+        { duration: '2m', target: 200 }, // normal load
+        { duration: '5m', target: 200 },
+        { duration: '2m', target: 300 }, // around the breaking point
+        { duration: '5m', target: 300 },
+        { duration: '2m', target: 400 }, // beyond the breaking point
+        { duration: '5m', target: 400 },
+        { duration: '10m', target: 0 }, // scale down. Recovery stage.
+    ],
+};
+const API_BASE_URL = 'http://20.103.147.242';
+//insert id to be tested on
+const userId = '';
+
+export default function ()
+{
+    const res = http.get(`${API_BASE_URL}/prescription/user/${userId}`);
+
+    check(res, {
+        'is status 200': (x) => x.status === 200
+    });
+
+    sleep(1);
+}
